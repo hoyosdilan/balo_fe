66f68075d2048fbe58f86b01a816fc05980cac22
Dilan Hoyos <hoyosdilan@gmail.com>
2023-10-29 14:14:11 -0500

    update the way to get data

--- src/app/data-scraper/data-scraper.service.ts
+++ src/app/data-scraper/data-scraper.service.ts
@@ -13,7 +13,7 @@ export class DataScraperService {
     let data = await this.getChartData(collections.QUANTITY_RESULTS_TRADITIONAL);
     const maxQuantity = Math.floor(Math.max(...data.map((e: any) => e.quantity)) + 1);
     const yrange = maxQuantity + (5 - maxQuantity % 5);
-    data = data.map((e: any) => ({value: e.quantity, label: e.key})).sort((a: any, b: any) => a.label - b.label);
+    data = data.map((e: any) => ({value: e.quantity, label: e._id})).sort((a: any, b: any) => a.label - b.label);
 
     return { lineData: data, yrange: yrange };
   }
@@ -22,7 +22,7 @@ export class DataScraperService {
     let data = await this.getChartData(collections.QUANTITY_RESULTS_REVANCHA);
     const maxQuantity = Math.floor(Math.max(...data.map((e: any) => e.quantity)) + 1);
     const yrange = maxQuantity + (5 - maxQuantity % 5);
-    data = data.map((e: any) => ({value: e.quantity, label: e.key})).sort((a: any, b: any) => a.label - b.label);
+    data = data.map((e: any) => ({value: e.quantity, label: e._id})).sort((a: any, b: any) => a.label - b.label);
 
     return { lineData: data, yrange: yrange };
   }
@@ -31,7 +31,7 @@ export class DataScraperService {
     let data = await this.getChartData(collections.QUANTITY_BALOTA_TRADITIONAL);
     const maxQuantity = Math.floor(Math.max(...data.map((e: any) => e.quantity)) + 1);
     const yrange = maxQuantity + (5 - maxQuantity % 5);
-    data = data.map((e: any) => ({value: e.quantity, label: e.key})).sort((a: any, b: any) => a.label - b.label);
+    data = data.map((e: any) => ({value: e.quantity, label: e._id})).sort((a: any, b: any) => a.label - b.label);
 
     return { lineData: data, yrange: yrange };
   }
@@ -40,47 +40,47 @@ export class DataScraperService {
     let data = await this.getChartData(collections.QUANTITY_BALOTA_REVANCHA);
     const maxQuantity = Math.floor(Math.max(...data.map((e: any) => e.quantity)) + 1);
     const yrange = maxQuantity + (5 - maxQuantity % 5);
-    data = data.map((e: any) => ({value: e.quantity, label: e.key})).sort((a: any, b: any) => a.label - b.label);
+    data = data.map((e: any) => ({value: e.quantity, label: e._id})).sort((a: any, b: any) => a.label - b.label);
 
     return { lineData: data, yrange: yrange };
   }
 
   public async sinceLastResultsTraditional(): Promise<ChartData> {
     let data = await this.getChartData(collections.RESULTS_SINCE_LAST_TRADITIONAL);
-    const maxQuantity = Math.floor(Math.max(...data.map((e: any) => e.since)) + 1);
+    const maxQuantity = Math.floor(Math.max(...data.map((e: any) => e.quantity)) + 1);
     const yrange = maxQuantity + (5 - maxQuantity % 5);
-    data = data.map((e: any) => ({value: e.since, label: e.number})).sort((a: any, b: any) => a.label - b.label);
+    data = data.map((e: any) => ({value: e.quantity, label: e._id})).sort((a: any, b: any) => a.label - b.label);
 
     return { lineData: data, yrange: yrange };
   }
 
   public async sinceLastResultsRevancha(): Promise<ChartData> {
     let data = await this.getChartData(collections.RESULTS_SINCE_LAST_REVANCHA);
-    const maxQuantity = Math.floor(Math.max(...data.map((e: any) => e.since)) + 1);
+    const maxQuantity = Math.floor(Math.max(...data.map((e: any) => e.quantity)) + 1);
     const yrange = maxQuantity + (5 - maxQuantity % 5);
-    data = data.map((e: any) => ({value: e.since, label: e.number})).sort((a: any, b: any) => a.label - b.label);
+    data = data.map((e: any) => ({value: e.quantity, label: e._id})).sort((a: any, b: any) => a.label - b.label);
 
     return { lineData: data, yrange: yrange };
   }
 
   public async sinceLastBalotaTraditional(): Promise<ChartData> {
     let data = await this.getChartData(collections.BALOTA_SINCE_LAST_TRADITIONAL);
-    const maxQuantity = Math.floor(Math.max(...data.map((e: any) => e.since)) + 1);
+    const maxQuantity = Math.floor(Math.max(...data.map((e: any) => e.quantity)) + 1);
     const yrange = maxQuantity + (5 - maxQuantity % 5);
-    data = data.map((e: any) => ({value: e.since, label: e.number})).sort((a: any, b: any) => a.label - b.label);
+    data = data.map((e: any) => ({value: e.quantity, label: e._id})).sort((a: any, b: any) => a.label - b.label);
 
     return { lineData: data, yrange: yrange };
   }
 
   public async sinceLastBalotaRevancha(): Promise<ChartData> {
     let data = await this.getChartData(collections.BALOTA_SINCE_LAST_REVANCHA);
-    const maxQuantity = Math.floor(Math.max(...data.map((e: any) => e.since)) + 1);
+    const maxQuantity = Math.floor(Math.max(...data.map((e: any) => e.quantity)) + 1);
     const yrange = maxQuantity + (5 - maxQuantity % 5);
-    data = data.map((e: any) => ({value: e.since, label: e.number})).sort((a: any, b: any) => a.label - b.label);
+    data = data.map((e: any) => ({value: e.quantity, label: e._id})).sort((a: any, b: any) => a.label - b.label);
 
     return { lineData: data, yrange: yrange };
   }
-  
+
   // fetch to localhost:3000/quantityResultsTraditional to get the data
   public getChartData(collection: collections): any {
     return new Promise((resolve, reject) => {
